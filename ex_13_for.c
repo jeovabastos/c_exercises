7d735b299dc0f6063398e74f8454a085db40b791
jeovabastos <jbastos.im@gmail.com>
2026-09-25 14:15:47 -0400

    feat: visualizar inventário com laço for

---
+++ ex_13_for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ITENS 5 // jeito mais simples que achei para usar o tamanho da array como controle do FOR
+
+int main(){
+    char *itens[MAX_ITENS] = {"sword", "healing_potion", "backpack", "shield", "clothes"};
+
+    printf("todos os itens do inventario: \n");
+
+    for(int i = 0; i < MAX_ITENS - 1; i++){
+        printf("%s\n", itens[i]);
+    }
+
+    printf("\né isso :'3\n");
+    return 0;
+}
